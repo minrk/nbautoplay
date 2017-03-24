6a8934d85c985c03d051b6da2391ed679e5726cf
Min RK <benjaminrk@gmail.com>
2017-03-24 12:56:42 +0100

    restart notebook after 60 seconds at end

--- autoplay.js
+++ autoplay.js
@@ -1,12 +1,13 @@
 require(["jquery", "base/js/namespace"], function($, Jupyter) {
   var Autoplay = function() {
     this.interval = null;
-    this.paused = false;
+    this._stopped = false;
+    this.step_ms = 10000;
+    this.restart_ms = 60000;
   };
 
   Autoplay.prototype.step = function() {
-    // don't do anything if explicitly paused
-    // or another window has focus
+    // don't do anything if another window has focus
     if (!document.hasFocus()) {
       console.debug("autoplay: out of focus");
       return;
@@ -22,37 +23,72 @@ require(["jquery", "base/js/namespace"], function($, Jupyter) {
       nb.get_selected_cell().get_text().trim() === ""
     ) {
       // at the end
-      console.debug("autoplay: at end");
+      this.schedule_restart();
     } else {
       Jupyter.notebook.execute_cell_and_select_below();
     }
   };
 
+  Autoplay.prototype.schedule_restart = function() {
+    // called when we reach the end
+    var autoplay = this;
+    this._stop();
+    console.debug("autoplay: scheduling restart");
+    setTimeout(
+      function() {
+        if (this._stopped) {
+          // someone else stopped us
+          console.debug("autoplay: aborting restart");
+          return;
+        }
+        console.debug("autoplay: restarting");
+        var nb = Jupyter.notebook;
+        nb.clear_all_output();
+        nb.select(0);
+        nb.restart_kernel({ confirm: false }).then(function() {
+          autoplay.start();
+        });
+      },
+      this.restart_ms,
+    );
+  };
+
   Autoplay.prototype.stop = function() {
+    this._stopped = true;
     if (this.interval) {
-      clearInterval(this.interval);
+      console.debug("autoplay: stopping");
+      this._stop();
     }
   };
 
-  Autoplay.prototype.start = function(ms) {
-    ms = ms || 5000;
+  Autoplay.prototype._stop = function() {
+    clearInterval(this.interval);
+    this.interval = null;
+  };
 
-    this.stop();
+  Autoplay.prototype.start = function(ms) {
+    ms = ms || this.step_ms;
+    this._stop();
+    console.debug("autoplay: starting");
     this.pasued = false;
-    var that = this;
+    var autoplay = this;
     this.interval = setInterval(
       function() {
-        that.step();
+        autoplay.step();
       },
       ms,
     );
   };
 
+  // create and start the global instance
+  // avoid duplicate instances:
   if (window.autoplay) {
     window.autoplay.stop();
   }
+
   window.autoplay = new Autoplay();
   window.autoplay.start();
+
   return {
     Autoplay: Autoplay,
   };
